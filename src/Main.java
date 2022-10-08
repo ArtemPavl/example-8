f287b7d02dddba8fbe81920d934e5daea8e7b1bb
ArtemPavl <artem.pavlenko.93@inbox.ru>
2022-10-08 21:10:30 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание к уроку 8");
-        System.out.println("Домашняя работа 2");
+        System.out.println("Домашняя работа 3");
         // Задание 1
         System.out.println("Задание 1");
-        int total = 0;
-        int percent = 7;
-        int totalFinish = 12_000_000;
-        int contribution = 15_000;
-        int month = 0;
-        while (total<=totalFinish){
-            total = total + total*percent/100;
-            total = total + contribution;
-            month++;
-            System.out.println("Месяц " + month + ", сумма накоплений равна "
-                    + total + " рублей");
-
-        }
+        int year = 0;
+        int yearNow = 2022;
+        do {
+            if (year >= yearNow - 200 && year <= yearNow + 100) {
+                System.out.println(year);
+            }
+            year = year + 79;
+        } while (year < 3000);
 
         // Задание 2
         System.out.println("Задание 2");
-        total = 0;
-        percent = 7;
-        totalFinish = 12_000_000;
-        contribution = 15_000;
-        month = 0;
-        while (total<=totalFinish) {
-            total = total + total * percent / 100;
-            total = total + contribution;
-            month++;
-            if (month % 6 == 0) {
-                System.out.println("Месяц " + month + ", сумма накоплений равна "
-                        + total + " рублей");
-            }
+        int number = 0;
+        int resoult;
+        for (int i = 1; i <= 10; i++) {
+            resoult = 2 * i;
+            System.out.println("2" + "*" + i + "=" + resoult);
         }
 
-            // Задание 3
-            System.out.println("Задание 3");
-            total = 0;
-            percent = 7;
-            int monthFinish = 9*12;
-            contribution = 15_000;
-            month = 0;
-            while (month<=monthFinish) {
-                total = total + total * percent / 100;
-                total = total + contribution;
-                month++;
-                if (month % 6 == 0) {
-                    System.out.println("Месяц " + month + ", сумма накоплений равна "
-                            + total + " рублей");
-                }
-            }
-
-        // Задание 4
-        System.out.println("Задание 4");
-        int friday = 3;
-        while (friday<=31) {
-            System.out.println("Сегодня пятница, " + friday
-                    + "-е число. Необходимо подготовить отчет.");
-            friday = friday + 7;
-        }
     }
-
 }
